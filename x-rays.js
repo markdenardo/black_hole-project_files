c9b4cff44a1b6bb5cdff49f3d1c4a31ecf1a1f8e
MDN <mark.denardo@gmail.com>
2022-09-12 13:03:16 -0400

    Create x-rays.js

---
+++ x-rays.js
@@ -0,0 +1,13 @@
+//black_hole: x-rays
+//MDN cc 2022
+const t =(()=>time)
+const pi=(()=>Math.random(Math.PI))
+const f=(()=>a.fft[0])
+const sh=(s,o,m)=>(shape(s).repeat(t,1).kaleid(t).scale(pi).rotate(t,0.1).out(o))
+const flt=(i,v,o)=>(src(i).scale(v).out(o))
+const flt2=(i,m,v,o)=>(src(i).modulate(m).modulatePixelate(noise(v,0.5),100).out(o))
+sh(pi,o0,o2)
+flt(o0,f,o1)
+flt(o0,pi,o2)
+flt2(o0,o1,f,o3)
+render(o0)
