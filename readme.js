3337424faf8793f9d452fed51a9c5558a8d9f774
MDN <mark.denardo@gmail.com>
2022-09-14 12:38:00 -0400

    Update readme.js

--- readme.js
+++ readme.js
@@ -8,8 +8,8 @@
 // two live code sessions, in Orca + OP-1 + hydra.js
 
 // SOURCE MATERIAL
-// themes inspired from childhood near-death experiences and how we came to understand them...
-// theory: we are all singularity material, we can make/build gravity wells, for whatever we want.
+// themes inspired from childhood near-death experiences and how we came to understand them
+// theory: we are all singularity material, we can make/build gravity wells, for whatever we want
 
 // VISUALS: to cast the visual component of this project apply these QR Codes:
 // <insert QR code here for x-rays>
