44fa2b2323b3fae021ca837b09fa40578010e629
MDN <mark.denardo@gmail.com>
2022-09-14 11:32:54 -0400

    Update x-rays.js

--- x-rays.js
+++ x-rays.js
@@ -1,13 +1,10 @@
-//black_hole: x-rays
-//MDN cc 2022
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+//black_hole: x-rays by MDN 
+//@markdenardo
+bpm =300
 const t =(()=>time)
 const pi=(()=>Math.random(Math.PI))
 const f=(()=>a.fft[0])
 const sh=(s,o,m)=>(shape(s).repeat(t,1).kaleid(t).scale(pi).rotate(t,0.1).out(o))
-const flt=(i,v,o)=>(src(i).scale(v).out(o))
-const flt2=(i,m,v,o)=>(src(i).modulate(m).modulatePixelate(noise(v,0.5),100).out(o))
-sh(pi,o0,o2)
-flt(o0,f,o1)
-flt(o0,pi,o2)
-flt2(o0,o1,f,o3)
+sh(pi,o0,o1)
 render(o0)
