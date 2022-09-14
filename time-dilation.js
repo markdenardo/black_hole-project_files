ec6be9db1ba4f2b5e24fe1ceba854ae2e93d315d
MDN <mark.denardo@gmail.com>
2022-09-14 12:09:47 -0400

    Update time-dilation.js

--- time-dilation.js
+++ time-dilation.js
@@ -1,12 +1,15 @@
-//black_hole: x-rays
-//MDN cc 2022
+//licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+//black_hole - time dilation
+//MDN 
+
 const t =(()=>time)
 const pi=(()=>Math.random(Math.PI))
 const f=(()=>a.fft[0])
-const sh=(s,o,m)=>(shape(s).repeat(t,1).kaleid(t).scale(pi).rotate(t,0.1).out(o))
+const sh=(s,o,m)=>(shape(s).repeat(t,1).kaleid([1,2,3].fast()).scale(pi).rotate(t,0.1).out(o))
 const flt=(i,v,o)=>(src(i).scale(v).out(o))
 const flt2=(i,m,v,o)=>(src(i).modulate(m).modulatePixelate(noise(v,0.5),100).out(o))
-sh(pi,o0,o2)
+sh(f,o0,o0)
 flt(o0,f,o1)
-flt2(o0,o1,f,o2)
-render(o0)
+flt(o0,pi,o2)
+flt2(o0,o1,f,o3)
+render(o3)
