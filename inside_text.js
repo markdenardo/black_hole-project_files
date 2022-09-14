69df48b1df84e8c6ff4d26deda3f1d112e57036a
MDN <mark.denardo@gmail.com>
2022-09-14 12:22:22 -0400

    Update inside_text.js

--- inside_text.js
+++ inside_text.js
@@ -1,24 +1,23 @@
 
 
-black_hole
-a poem about a dance with a black hole.
-made w code by MDN 
+// black_hole
+// a spell re: a dance with a black hole.
+// made w code by MDN 
 
-side a: x-rays
-side b: time_dilation
+// side a: x-rays
+// side b: time_dilation
 
-two live code sessions, in Orca + OP-1 + hydra.js
+// two live code sessions, in Orca + OP-1 + hydra.js
 
-SOURCE MATERIAL
-themes inspired from childhood near-death experiences and how we came to understand them...
-theory: we are all singularity material, we can make/build gravity wells, for whatever we want.
+// SOURCE MATERIAL
+// themes inspired from childhood near-death experiences and how we came to understand them...
+// theory: we are all singularity material, we can make/build gravity wells, for whatever we want.
 
-<PLACEHOLDER hydra.js WEB link>
-when you go to this page, it can serve as a visualizer for the bh tape, 
-or it can be used as a custom made hydra.js patch in our design.
+// VISUALS: to cast the visual component of this project apply these QR Codes:
+// We encourage you to explore the code of these projects, and share what you make.
 
-Thank you!!
-CS, TE, KB, MNP, OJ
+// Thank you !!
+// Special thanks ()=> (CS, TE, KB, MNP, OJ)
 
-all songs, code, and ideas 
-by MDN / Mark DeNardo 2022
+// all songs, code, and ideas 
+// by MDN / Mark DeNardo 2022
