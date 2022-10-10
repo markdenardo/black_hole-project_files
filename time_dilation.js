84ace6bc1ca939ea5147dcabe664c413beb8971c
MDN <mark.denardo@gmail.com>
2022-10-10 12:47:51 -0400

    Update time_dilation.js

--- time_dilation.js
+++ time_dilation.js
@@ -2,12 +2,20 @@
 // black_hole - time_dilation
 // MDN (@markdenardo)
 
+perms = 0;
+function cM(){navigator.permissions.query({name: 'microphone'}).then((pS)=>{perms = pS.state !== 'denied';});}
+cI = window.cI ? cI : 0;clearInterval(cI);cI = setInterval(cM,3000);if(/iPad|iPhone|iPod/.test(navigator.userAgent)) clearInterval(cI);
+
+let nomic = ()=> Math.sin(time*4);
+let yesmic = ()=> a.fft[0];
+let f = ()=> perms ? yesmic() : nomic();
+
 const t =(()=>time)
 const pi=(()=>Math.random(Math.PI))
-const f=(()=>a.fft[0])
 const sh=(s,o,m)=>(shape(s).repeat(t,1).kaleid([1,2,3].fast()).scale(pi).rotate(t,0.1).out(o))
 const flt=(i,v,o)=>(src(i).scale(v).out(o))
 const flt2=(i,m,v,o)=>(src(i).modulate(m).modulatePixelate(noise(v,0.5),100).out(o))
+
 sh(f,o0,o0)
 flt(o0,f,o1)
 flt(o0,pi,o2)
