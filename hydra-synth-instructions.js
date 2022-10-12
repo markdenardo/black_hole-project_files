923a55f6fc9b7caa5f1976386c641647f80d8e28
MDN <mark.denardo@gmail.com>
2022-10-11 23:09:10 -0400

    Update hydra-synth-instructions.js

--- hydra-synth-instructions.js
+++ hydra-synth-instructions.js
@@ -2,25 +2,25 @@
 //
 // hydra-synth is a video synth made by Olivia Jack (IG: @o_jack)
 //
-// accessing the QR code will open the projects in a web page
+// accessing the QR code will open bh projects in a web page
 //
 // the x-rays project will run as soon as you load the page
 //
-// the time_dilation project responds dynamically to the composition
+// the time_dilation project responds dynamically to the sound composition
 //
-// allow microphone access on your web browser of choice so that you can view the visuals
-// there is default code if mic permissions are not allowed
+// caveat: allow microphone access on your web browser of choice so that you can view the visuals
+// default code will run if mic permissions are not turned on
 //
 // SOME TO-DOS: 
-// change the buffer by assigning o0, o1,o2,o3 as an argument within the render() function
-// example: 
-// render(o0)
-// change the arguments within the project, accessing the variables assigned
+// 1. change the buffer by assigning o0, o1,o2,o3 as an argument within the render() function
+//    example: 
+//    render(o0)
+//
+// 2. change the arguments within the project, accessing the variables assigned
+//    example: 
+//    sh(pi,o1,o1)
+//    flt(o2,o1,pi,o3)
 // 
-// example: 
-// sh(pi,o1,o1)
-// flt(o2,o1,pi,o3)
-// 
-// play around and have fun! if you are interested in learning more about hydra, 
+// play around and have fun! 
 // check out the repo: 
 // https://github.com/hydra-synth/hydra
