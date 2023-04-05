a01bb17b1cbe520bcb7c6781afb30805afe81162
MDN <mark.denardo@gmail.com>
2023-04-05 12:28:04 -0400

    Update x-rays.js

--- x-rays.js
+++ x-rays.js
@@ -2,10 +2,15 @@
 // black_hole - x-rays 
 // MDN (@markdenardo)
 
-bpm=300
-const t=(()=>time)
-const pi=(()=>Math.random(Math.PI))
-const f=(()=>a.fft[0])
-const sh=(s,o,m)=>(shape(s).repeat(t,1).kaleid(t).scale(pi).rotate(t,0.1).out(o))
-sh(pi,o0,o1)
+bpm = 300
+const t = (() => time)
+const pi = (() => Math.random(Math.PI))
+const f = (() => a.fft[0])
+const sh = (s, o, m) => (shape(s)
+	.repeat(t, 1)
+	.kaleid(t)
+	.scale(pi)
+	.rotate(t, 0.1)
+	.out(o))
+sh(pi, o0, o1)
 render(o0)
