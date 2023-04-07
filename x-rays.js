3721b2dd62b10ed86da18d676de9d8f2ecc5091a
MDN <mark.denardo@gmail.com>
2023-04-07 15:40:30 -0400

    Update x-rays.js

--- x-rays.js
+++ x-rays.js
@@ -1,8 +1,10 @@
-// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+// licensed with CC BY-NC-SA 4.0 
+// https://creativecommons.org/licenses/by-nc-sa/4.0/
 // black_hole - x-rays 
 // MDN (@markdenardo)
 
 bpm = 300
+
 const t = (() => time)
 const pi = (() => Math.random(Math.PI))
 const f = (() => a.fft[0])
@@ -12,5 +14,6 @@ const sh = (s, o, m) => (shape(s)
 	.scale(pi)
 	.rotate(t, 0.1)
 	.out(o))
+
 sh(pi, o0, o1)
 render(o0)
